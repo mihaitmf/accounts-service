117d28bb8b57eda7e2109ed38c9260e4ad30b821
mihaitmf <mihai.tmf@gmail.com>
2015-06-29 19:06:24 +0300

    Add dummy controller test

---
+++ src/test/java/accounts/controller/AccountControllerTest.java
@@ -0,0 +1,14 @@
+package accounts.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AccountControllerTest {
+
+    @Test
+    public void shouldCreateAccount() {
+
+    }
+}
