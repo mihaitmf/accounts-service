e04854c8c89a132b4fe2fba4f460ea2738a8b063
mihaitmf <mihai.tmf@gmail.com>
2015-07-01 21:45:31 +0300

    Add simple Account entity

--- src/main/java/accounts/controller/AccountController.java
+++ src/main/java/accounts/controller/AccountController.java
@@ -1,5 +1,7 @@
 package accounts.controller;
 
+import accounts.model.Account;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     @RequestMapping(value = "/account", method = RequestMethod.POST)
-    public String create() {
-        return "hello";
+    public Account create(@RequestBody Account account) {
+
+        System.out.println("################## controller #########################");
+        System.out.println(account);
+        System.out.println("################## controller end #########################");
+
+        return account;
     }
 }
