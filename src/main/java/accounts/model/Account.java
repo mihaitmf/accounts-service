8c54cc4666ed94998831377b6b514a5004cb79dc
mihaitmf <mihai.tmf@gmail.com>
2015-07-17 17:41:10 +0300

    Add repository interface and liquibase changelog

--- src/main/java/accounts/model/Account.java
+++ src/main/java/accounts/model/Account.java
@@ -1,7 +1,37 @@
 package accounts.model;
 
-import javax.persistence.Entity;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 
-//@Entity
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "accounts")
 public class Account {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "AccountId")
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Email
+    @Column(name = "Email", unique = true, nullable = false, updatable = false)
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 6)
+    @Column(name = "Password")
+    private String password;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Address")
+    private String address;
+
 }
---
+++ src/main/java/accounts/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package accounts.repository;
+
+import accounts.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
